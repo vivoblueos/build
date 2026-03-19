fe6900f7101396b3013dd0c1f94364a304c91986
xuchang-vivo <xuchang0929@vivo.com>
2026-03-19 14:07:49 +0800

    fix bug

--- scripts/reload_autoconf_and_build.py
+++ scripts/reload_autoconf_and_build.py
@@ -14,9 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-This file will reload kconfig, modify .config again, and call 
-ninja to recompile the target, rebuilding the dependency tree.
-Note: This script will modified the .config globally, so it should be used carefully.
+This file will build a new gn project from the parent project.
 """
 
 import os
@@ -34,29 +32,42 @@ def reload_kconfig(kconfig_path, autoconf_path, app_conf_path):
     kconf.write_config(autoconf_path)
 
 
+def gen_ninja_workspace(ninja_dir, board, build_type, defconfig_files):
+    gn_cmd = ['gn', 'gen', ninja_dir]
+    gn_cmd += [
+        f'--args=board=\"{board}\" defconfig_files=\"{defconfig_files}\" build_type=\"{build_type}\"'
+    ]
+    subprocess.run(gn_cmd, check=True)
+
+
+def build_with_ninja(ninja_dir, target_name):
+    ninja_cmd = ['ninja', '-C', ninja_dir, target_name]
+    subprocess.run(ninja_cmd, check=True)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--kconfig", help="Kconfig path")
+    parser.add_argument("--defconfig_files", help="defconfig files")
     parser.add_argument("--board", help="target board")
-    parser.add_argument("--autoconf", help="config file path")
-    parser.add_argument("--app_conf",
-                        help="app config file path",
-                        default=None)
-    parser.add_argument("-C", "--ninja_dir", help="ninja build directory")
+    parser.add_argument("--build_type", help="build type")
+    parser.add_argument("--ninja_dir", help="ninja build directory")
+    parser.add_argument("--output_dir", help="output dir path")
     parser.add_argument("target_name", help="ninja target name")
     args = parser.parse_args()
-    os.environ["BOARD"] = args.board
-    os.environ["KCONFIG_DIR"] = os.path.dirname(args.kconfig)
-    kconfig_dir = os.path.dirname(args.kconfig)
-    kernel_src_dir = os.path.join(
-        os.path.dirname(os.path.dirname(kconfig_dir)), "kernel", "src")
-    os.environ["KERNEL_SRC_DIR"] = os.path.abspath(kernel_src_dir)
-    try:
-        if args.app_conf:
-            reload_kconfig(args.kconfig, args.autoconf, args.app_conf)
 
-        ninja_cmd = ['ninja', '-C', args.ninja_dir, args.target_name]
-        subprocess.run(ninja_cmd, check=True)
+    try:
+        gen_ninja_workspace(args.ninja_dir, args.board, args.build_type,
+                            args.defconfig_files)
+        build_with_ninja(args.ninja_dir, args.target_name)
+        target_name = args.target_name[:len("_pass")]
+        bin_file = args.ninja_dir + '/bin/' + args.target_name + '.bin'
+        cped_bin_file = args.output_dir + '/' + target_name + '.bin'
+        elf_file = args.ninja_dir + '/bin/' + args.target_name
+        cped_elf_file = args.output_dir + '/' + target_name
+        cp_cmd = ['cp', bin_file, cped_bin_file]
+        subprocess.run(cp_cmd, check=True)
+        cp_cmd = ['cp', elf_file, cped_elf_file]
+        subprocess.run(cp_cmd, check=True)
 
     except Exception as e:
         print(f"Error reload autoconf and build: {e}")
